8e872e05def19fbe92b941b0d3d10c21a7dc99f5
mgiovenco <michael.e.giovenco@gmail.com>
2021-02-20 16:24:43 -0600

    Initial commit of beer-client-service

---
+++ src/main/java/com/beer/beerclientservice/BeerClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beer.beerclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerClientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beer/beerclientservice/BeerClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer.beerclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
